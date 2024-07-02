09d4795e998dbde004ff375f6b6290e83129a9b7
jokrasa1011 <jonas.kreutz.01@gmail.com>
2024-07-02 20:29:42 +0200

    added poetry --no-update

--- arena/arena-rosnav/setup/install2/install2.sh
+++ arena/arena-rosnav/setup/install2/install2.sh
@@ -34,6 +34,7 @@ ln -s src/arena/arena-rosnav/setup/install2/* src/arena/arena-rosnav/
 cd src/arena/arena-rosnav
 export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring # resolve faster
 poetry run poetry install --no-root
+poetry lock --no-update
 poetry env use python3.8
 . "$(poetry env info -p)/bin/activate"
 cd ${TARGET_DIR}
